687546ea11c4f11ab7e5e9c1e70fa22e94cdb9d5
florian <34652@ma-web.nl>
2023-09-06 11:10:44 +0200

    player movement florian

---
+++ GameJamGameGame/Assets/flo/player/movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) && transform.position.x < 7.5)
+        {
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, 0, -12);
+        }
+        else if (Input.GetKey(KeyCode.A) && transform.position.x > -7.5)
+        {
+            transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, 0, 12);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
